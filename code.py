16c516d4b60ced3284e8bc7752906992822dcdb2
PetrKupka1 <107040662+PetrKupka1@users.noreply.github.com>
2022-06-08 14:08:32 +0200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,8 @@
+import requests
+substitute = "Homer"
+
+requests.post(
+    'https://api.telegram.org/bot5376100113:AAG45U2UdoIr4vjtrs37rUXDtf6N3TQRTEU/sendMessage?chat_id=-763937108&text=Yello %s' % substitute)
+
+
+
